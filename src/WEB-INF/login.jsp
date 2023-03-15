4d99e7a8f4c555c7fc4c9b312635405f1a9a53b1
Randy Chu <randychu7@gmail.com>
2023-03-14 19:08:42 -0500

    finished

--- src/WEB-INF/login.jsp
+++ src/WEB-INF/login.jsp
@@ -1,14 +1,15 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <jsp:include page="partials/head.jsp">
+    <jsp:include page="../main/webapp/partials/head.jsp">
         <jsp:param name="title" value="Please Log In" />
     </jsp:include>
 </head>
 <body>
-    <jsp:include page="partials/navbar.jsp" />
+    <jsp:include page="../main/webapp/partials/navbar.jsp" />
     <div class="container">
         <h1>Please Log In</h1>
+
         <form action="/login" method="POST">
             <div class="form-group">
                 <label for="username">Username</label>
--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,21 +3,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login1")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user")== null){
+            response.sendRedirect("/login");
+            return;
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        session.setAttribute("user",username);
+
+        String user = (String) session.getAttribute("username");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
-            response.sendRedirect("/profile");
+            request.getSession().setAttribute("user",user);
+            request.getRequestDispatcher("/profile").forward(request,response);
         } else {
             response.sendRedirect("/login");
         }
